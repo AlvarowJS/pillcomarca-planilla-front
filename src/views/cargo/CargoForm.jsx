fc36b5808637e7b364b9f4f4476eb4cbca0e7bba
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2025-04-28 23:35:38 -0500

    avance de cargo

--- src/views/cargo/CargoForm.jsx
+++ src/views/cargo/CargoForm.jsx
@@ -12,33 +12,52 @@ const CargoForm = ({
 }) => {
   return (
     <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader>Registro de Cargos</ModalHeader>
-        <ModalBody>
-            <form onSubmit={handleSubmit(submit)}>
-            <div className="form-group">
-                <div>
-                <label>Cargo</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Ingrese el nombre del Cargo"
-                    {...register("nombre")}
-                />
-                </div>
-                <br />
-                <div>
-                <label>Descripcion</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Ingrese su Descripccion"
-                    {...register("descripcion")}
-                />
-                </div>
+      <ModalHeader>Registro de Cargos</ModalHeader>
+      <ModalBody>
+        <form onSubmit={handleSubmit(submit)}>
+          <div className="form-group">
+            <div>
+              <label>Cargo</label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Ingrese el nombre del Cargo"
+                {...register("nombre")}
+              />
             </div>
             <br />
-            <button className="btn btn-primary">Guardar</button>
-            </form>
+            <div>
+              <label>Clasificador</label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Ingrese su Descripccion"
+                {...register("clasificador")}
+              />
+            </div>
+            <div>
+              <label>Código</label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Ingrese su Descripccion"
+                {...register("codigo")}
+              />
+            </div>
+            <div>
+              <label>Descripcion</label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Ingrese su Descripccion"
+                {...register("descripcion")}
+              />
+            </div>
+      
+          </div>
+          <br />
+          <button className="btn btn-primary">Guardar</button>
+        </form>
       </ModalBody>
     </Modal>
   );
